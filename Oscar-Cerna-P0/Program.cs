3917b865fb7fb1620227261349682c2579054254
OzzyOzy <oscarcerna1992@icloud.com>
2021-10-06 18:40:12 -0400

    Project 0

---
+++ Oscar-Cerna-P0/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Oscar_Cerna_P0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
